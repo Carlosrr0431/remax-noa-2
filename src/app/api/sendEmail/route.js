c213a000c453acb6faf0f239705574e2c811e11f
Carlos RR <carlos.facundo.rr@gmail.com>
2024-09-26 11:53:21 -0300

    mensaje 3

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -31,8 +31,8 @@ export async function POST(request) {
         name: "Carlos RR",
         address: "carlos.facundo.rr@gmail.com",
       },
-      // to: ["giu40150135@gmail.com", "carlos.facundo.rr@gmail.com"],
-      to: email,
+      to: ["giu40150135@gmail.com", "carlos.facundo.rr@gmail.com"],
+      // to: email,
       subject: "Mensaje de Marketing",
       html: `
  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="format-detection" content="telephone=no"><title>Nueva plantilla</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style>
